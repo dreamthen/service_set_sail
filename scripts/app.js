27c275595e00eaa09915d6297cf364f371128fc1
尹文楷 <yinwk@servyou.com.cn>
2018-04-03 21:27:38 +0800

    add reducer and storeConfig

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,9 @@ import React from "react";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
 import {Router, browserHistory} from "react-router";
+import store from "./configs/appStoreConfig";
 
-render(<Provider store={}>
+render(<Provider store={store}>
         <Router history={browserHistory}>
 
         </Router>
--- scripts/configs/appStoreConfig.js
+++ scripts/configs/appStoreConfig.js
@@ -2,6 +2,7 @@ import {createStore, applyMiddleware, compose} from "redux";
 import thunkMiddleware from "redux-thunk";
 import loggerMiddleware from "redux-logger";
 import {persistState} from "redux-devtools";
+import reducers from "../reducers";
 
 const production = process.env.NODE_ENV;
 let middleware = [];
@@ -13,4 +14,13 @@ if(production === PRODUCTION) {
 }else {
     middleware = [...middleware, thunkMiddleware, loggerMiddleware];
 }
+
+const compose_store = compose([
+    applyMiddleware(...middleware),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+])(createStore);
+
+const store = compose_store(reducers, {});
+
+export default store;
 
